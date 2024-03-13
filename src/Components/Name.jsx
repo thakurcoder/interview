52fc8489cdacf68655be64b4ac7de33ec3eaf636
Aman Kumar Singh <singh123aman12345@gmail.com>
2024-03-13 22:00:35 +0530

    make carousel

--- src/Components/Name.jsx
+++ src/Components/Name.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Name(props) {
+
   const data = props?.data;
+  const [show,setshow] = useState(true)
+  const handleclick = ()=>{
+    setshow(!show)
+  }
+
   return (
-    <div>
-    <h1>{data?.name}</h1>
-    {data?.child && <div style={{ marginLeft: 30, paddingLeft: 30 }}>
+    <div className='font-bold text-2xl p-7 '>
+    <h1 onClick={handleclick} className='shadow-lg p-5' > ⬇️ {data?.name}</h1>
+    {data?.child && show && <div className='ml-52 '>
         {data.child.map(e => {
             return <Name data={e} />
         })}
